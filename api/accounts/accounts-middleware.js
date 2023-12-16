5861dadfea2d79a580c8b6adbfb81e8e9da42104
Calvin K <calvinknaub@gmail.com>
2023-12-16 13:22:39 -0800

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,8 +1,21 @@
 const Account = require("./accounts-model");
 
 exports.checkAccountPayload = (req, res, next) => {
-  console.log("checkAccountPayload");
-  next();
+  const error = { status: 400 };
+  const { name, budget } = req.body;
+  if (name === undefined || budget === undefined) {
+    error.message = "name and budget are required";
+    next(error);
+  } else if (typeof name !== "string") {
+    error.message = "name of account must be a string";
+    next(error);
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = "name of account must be between 3 and 100";
+    next(error);
+  } else if (typeof budget !== "number" || !isNaN(budget)) {
+    error.message = "budget of account me be a number";
+    next(error);
+  }
 };
 
 exports.checkAccountNameUnique = (req, res, next) => {
@@ -14,7 +27,7 @@ exports.checkAccountId = async (req, res, next) => {
   try {
     const account = await Account.getById(req.params.id);
     if (!account) {
-      next({ status: 404, message: "not found" });
+      next({ status: 404, message: "account not found" });
     } else {
       req.account = account;
       next();
