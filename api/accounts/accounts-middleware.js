ee159067db216256fa6da2764386bacbe0382586
Calvin K <calvinknaub@gmail.com>
2023-12-16 12:54:21 -0800

    wip

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,14 @@
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  // Note: you can either write "manual" validation logic
-  // or use the Yup library (not currently installed)
+  console.log("checkAccountPayload");
+  next()
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+  console.log("checkAccountNameUnique");
+  next()
 }
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
+  console.log("checkAccountId");
+  next()
 }
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const md = require("./accounts-middleware");
 
 router.get("/", (req, res, next) => {
   try {
@@ -8,7 +9,7 @@ router.get("/", (req, res, next) => {
   }
 });
 
-router.get("/:id", (req, res, next) => {
+router.get("/:id", md.checkAccountId, (req, res, next) => {
   try {
     res.json("get accounts by id");
   } catch (error) {
@@ -16,23 +17,34 @@ router.get("/:id", (req, res, next) => {
   }
 });
 
-router.post("/", (req, res, next) => {
-  try {
-    res.json("post accounts");
-  } catch (error) {
-    next(error);
+router.post(
+  "/",
+  md.checkAccountId,
+  md.checkAccountNameUnique,
+  (req, res, next) => {
+    try {
+      res.json("post accounts");
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.put("/:id", (req, res, next) => {
-  try {
-    res.json("update accounts");
-  } catch (error) {
-    next(error);
+router.put(
+  "/:id",
+  md.checkAccountId,
+  md.checkAccountNameUnique,
+  md.checkAccountPayload,
+  (req, res, next) => {
+    try {
+      res.json("update accounts");
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", md.checkAccountId, (req, res, next) => {
   try {
     res.json("delete accounts");
   } catch (error) {
